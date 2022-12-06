35d9db3f958a18827ab1a515e0304d2b63d05b16
Cyrille <cypierre58@gmail.com>
2022-12-06 16:08:54 +0100

    Create ros zsh config file

---
+++ zsh/zsh/cfg/ros.zsh
@@ -0,0 +1,28 @@
+source_ros() { 
+	source ~/projects/ros/devel/setup.zsh
+	export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}'
+}
+
+source_isibot() {
+	source ~/projects/ros_isibot/local/devel/setup.zsh
+	export ROSCONSOLE_FORMAT='[${severity}] [${node}]: ${message}'
+}
+
+source_tiara() {
+	source ~/projects/tiara_ws/devel/setup.zsh
+	export ROSCONSOLE_FORMAT='[${severity}] ${node}: ${message}'
+	export GAZEBO_RESOURCE_PATH="$HOME/data/tiara/gazebo"
+	export GAZEBO_MODEL_PATH="$HOME/data/tiara/gazebo/models"
+}
+
+source_tiara2() {
+  unset ROS_DISTRO
+  source /opt/ros/galactic/setup.zsh
+  source ~/projects/tiara_ros2/install/local_setup.zsh
+  source /usr/share/colcon_cd/function/colcon_cd.sh
+  eval "$(register-python-argcomplete3 ros2)"
+  eval "$(register-python-argcomplete3 colcon)"
+  export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
+	export GAZEBO_RESOURCE_PATH="$HOME/data/tiara/gazebo"
+	export GAZEBO_MODEL_PATH="$HOME/data/tiara/gazebo/models"
+}
