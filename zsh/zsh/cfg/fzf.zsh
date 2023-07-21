651a506aedd53be7859fc049a2eb265e5d81394f
Cyrille Pierre <cyrille.pierre@inrae.fr>
2023-07-21 12:31:01 +0200

    Create fzf.zsh file

---
+++ zsh/zsh/cfg/fzf.zsh
@@ -0,0 +1,18 @@
+if [ -d ~/share/fzf/ ] ; then
+  source ~/share/fzf/shell/completion.zsh
+  source ~/share/fzf/shell/key-bindings.zsh
+
+  # Use fd (https://github.com/sharkdp/fd) instead of the default find
+  # command for listing path candidates.
+  # - The first argument to the function ($1) is the base path to start traversal
+  # - See the source code (completion.{bash,zsh}) for the details.
+  _fzf_compgen_path() {
+    fd --hidden --follow --exclude ".git" . "$1"
+  }
+
+  # Use fd to generate the list for directory completion
+  _fzf_compgen_dir() {
+    fd --type d --hidden --follow --exclude ".git" . "$1"
+  }
+
+fi
