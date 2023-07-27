a3798b75fb1efee6172772dc1c1224ae2b2ec8b2
Cyrille Pierre <cyrille.pierre@inrae.fr>
2023-07-27 18:18:59 +0200

    [vim] Use fzf.vim instead of fzf-preview.vim

--- zsh/zsh/cfg/fzf.zsh
+++ zsh/zsh/cfg/fzf.zsh
@@ -1,13 +1,16 @@
 cfg_fzf_dir='/opt/fzf'
 
 if [ -d "${cfg_fzf_dir}" ] ; then
+  local fzf_regfile_preview_command='bat --theme=gruvbox-dark --style=numbers --color=always --line-range :100 {}'
+  local fzf_dir_preview_command='fd --color=always --maxdepth 2 . {}'
+  export FZF_PREVIEW_COMMAND="(${fzf_regfile_preview_command} || ${fzf_dir_preview_command})"
+  export FZF_COMPLETION_OPTS="--preview '${FZF_PREVIEW_COMMAND} 2>/dev/null'"
+  export FZF_CTRL_T_OPTS="--preview '${FZF_PREVIEW_COMMAND} 2>/dev/null'"
+  export FZF_ALT_C_OPTS="--preview '${fzf_dir_preview_command} 2>/dev/null'"
+
   source "${cfg_fzf_dir}/shell/completion.zsh"
   source "${cfg_fzf_dir}/shell/key-bindings.zsh"
 
-  # Use fd (https://github.com/sharkdp/fd) instead of the default find
-  # command for listing path candidates.
-  # - The first argument to the function ($1) is the base path to start traversal
-  # - See the source code (completion.{bash,zsh}) for the details.
   _fzf_compgen_path() {
     fd --hidden --follow --exclude ".git" . "$1"
   }
