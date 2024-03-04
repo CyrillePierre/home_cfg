df9bfb28372b1a8535d4ef70bed9429446d3bc22
Cyrille <cypierre58@gmail.com>
2024-03-05 00:09:01 +0100

    [nvim] Fix vimas_sessions_dir

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -2,6 +2,11 @@ return {
 	{
 		'celforyon/vimas',
 		dependencies = {'tpope/vim-obsession'},
+
+		init = function()
+			vim.g.vimas_sessions_dir = vim.fn.stdpath('state') .. '/sessions'
+		end,
+
 		config = function()
 			os.execute('mkdir -p ' .. vim.fn.stdpath('state') .. '/sessions')
 			vim.loop.new_timer():start(0, 0, vim.schedule_wrap(function()
