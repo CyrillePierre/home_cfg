2e708fef03179311ddba2e15c4c76ea00583524f
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-03-12 14:56:45 +0100

    [nvim] neo-tree: always show src

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -3,10 +3,30 @@ return {
 		'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
 		cmd = 'Neotree',
+
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
     },
+
+		config = function()
+			require('neo-tree').setup{
+				filesystem = {
+					filtered_items = {
+						visible = false, -- when true, they will just be displayed differently than normal items
+						hide_dotfiles = true,
+						hide_gitignored = true,
+						hide_by_pattern = {
+							--"*.meta",
+							--"*/src/*/tsconfig.json",
+						},
+						always_show = { -- remains visible even if other settings would normally hide it
+							'src',
+						},
+					},
+				}
+			}
+		end,
 	},
 }
