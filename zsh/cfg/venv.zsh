be137813a2670e2bcf12cd0326a763e97215d8c6
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-05-22 09:39:37 +0200

    [python] Fix venv deactivate

--- zsh/cfg/venv.zsh
+++ zsh/cfg/venv.zsh
@@ -2,15 +2,11 @@ update_venv() {
   venv_dirname='.venv'
 
   # automatically deactivate when leaving a (sub-)directory containing a virtual env
-  # echo "pwd:  ${PWD:P}"
-  # echo "venv: ${VIRTUAL_ENV:P:h}"
-  if [[ -v ${VIRTUAL_ENV} && ! ${PWD:P}/ = ${VIRTUAL_ENV:P:h}/* ]] ; then
-    echo "deactivate"
+  if [[ -v VIRTUAL_ENV && ${PWD:P}/ != ${VIRTUAL_ENV:P:h}/* ]] ; then
     deactivate
   fi
 
   if [[ -d "${venv_dirname}" ]] ; then
-    echo "activate venv: ${PWD}/${venv_dirname}"
     source "${venv_dirname}/bin/activate"
   fi
 }
