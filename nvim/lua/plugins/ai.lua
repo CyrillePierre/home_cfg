12da504847eaa80808dcede4a8a5205267f3a0b2
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-08-13 14:17:00 +0200

    [nvim] Use ChatGPT4o-mini as default

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -3,6 +3,8 @@ return {
     'robitx/gp.nvim',
     opts = {
       openai_api_key = {'bw', 'get', 'password', 'gp.nvim_openai'},
+      default_command_agent = 'CodeGPT4o-mini',
+      default_chat_agent = 'ChatGPT4o-mini',
     },
     event = 'VeryLazy',
   },
