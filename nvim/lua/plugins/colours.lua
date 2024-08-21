bdcef05fd1e7bc64897a32ff37373ce8ba837ca2
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-08-21 17:36:58 +0200

    [nvim] Change ccc config (virtual text)

--- nvim/lua/plugins/colours.lua
+++ nvim/lua/plugins/colours.lua
@@ -1,3 +1,18 @@
 return {
-	{'uga-rosa/ccc.nvim', config = true},
+	{
+    'uga-rosa/ccc.nvim',
+    config = function ()
+      local ccc = require('ccc')
+      ccc.setup {
+        inputs = {
+          ccc.input.hsl,
+          ccc.input.rgb,
+          ccc.input.cmyk,
+        },
+        highlight_mode = "virtual",
+        virtual_symbol = "▐██▌",
+        virtual_pos = "inline-right",
+      }
+    end,
+  },
 }
